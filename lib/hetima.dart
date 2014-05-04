c4df338f071395e81f5253544424aeb8c2a011dd
kyorohiro <kyorohiro@gmail.com>
2014-05-04 18:50:42 +0900

    add array builder: variable byte array

--- lib/hetima.dart
+++ lib/hetima.dart
@@ -3,6 +3,8 @@ import 'dart:core' as core;
 import 'dart:typed_data' as data;
 import 'dart:html' as html;
 import 'dart:math' as math;
+import 'dart:convert' as convert;
 part 'util/bencode.dart';
 part 'util/uuid.dart';
+part 'util/arraybuilder.dart';
 part 'signal/caller.dart';
---
+++ lib/util/arraybuilder.dart
@@ -0,0 +1,37 @@
+part of hetima;
+
+class ArrayBuilder {
+  data.Uint8List _buffer8 = new data.Uint8List(1024);
+  core.int _length = 0;
+
+  void clear() {
+    _length = 0;
+  }
+
+  core.int size() {
+    return _length;
+  }
+
+  void appendString(core.String text) {
+    core.List<core.int> code = convert.UTF8.encode(text);
+    for (core.int i = 0; i < code.length; i++) {
+      _buffer8[_length] = code[i];
+      _length += 1;
+    }
+  }
+
+  void appendUint8List(data.Uint8List buffer, core.int index, core.int length) {
+    for (core.int i = 0; i < length; i++) {
+      _buffer8[_length] = buffer[index + i];
+      _length += 1;
+    }
+  }
+
+  core.List toList() {
+    return _buffer8.sublist(0, _length);
+  }
+
+  core.String toText() {
+    return convert.UTF8.decode(toList());
+  }
+}
