64ed039086b714f2d190e83f61333b3a77803d63
kyorohiro <kyorohiro@gmail.com>
2014-06-22 01:46:55 +0900

    ..

--- bin/httpurl_test.dart
+++ bin/httpurl_test.dart
@@ -11,7 +11,11 @@ void main() {
      unit.expect(url.port, 80);
      unit.expect(url.path, "");
   });
-
+  unit.test("127.0.0.1", () {
+     hetima.HttpUrlDecoder decoder = new hetima.HttpUrlDecoder();
+     hetima.HttpUrl url = decoder.decodeUrl("127.0.0.1");
+     unit.expect(url, null);
+  });
   unit.test("https://www.google.com:8080", () {
      hetima.HttpUrlDecoder decoder = new hetima.HttpUrlDecoder();
      hetima.HttpUrl url = decoder.decodeUrl("https://www.google.com:8080");
@@ -20,9 +24,20 @@ void main() {
      unit.expect(url.port, 8080);
      unit.expect(url.path, "");
   });
+
   unit.test("https://google.com:18080/xxx?sdfsdf=%01%02&aasdf_", () {
      hetima.HttpUrlDecoder decoder = new hetima.HttpUrlDecoder();
      hetima.HttpUrl url = decoder.decodeUrl("https://google.com:18080/xxx?sdfsdf=%01%02&aasdf_");
+     unit.expect(url.scheme, "https");
+     unit.expect(url.host, "google.com");
+     unit.expect(url.port, 18080);
+     unit.expect(url.path, "/xxx?sdfsdf=%01%02&aasdf_");
+  });
+
+  // todo error にすべき？
+  unit.test("https://google.com:18080/xxx?sdfsdf=%01%02&aasdf_あ", () {
+     hetima.HttpUrlDecoder decoder = new hetima.HttpUrlDecoder();
+     hetima.HttpUrl url = decoder.decodeUrl("https://google.com:18080/xxx?sdfsdf=%01%02&aasdf_あ");
      unit.expect(url.scheme, "https");
      unit.expect(url.host, "google.com");
      unit.expect(url.port, 18080);
