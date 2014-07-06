bee884f53d9c99f4557600059f33f70923d9ea9f
kyorohiro <kyorohiro@gmail.com>
2014-07-06 21:15:18 +0900

    ..

---
+++ bin/httpserver_sample.dart
@@ -0,0 +1,7 @@
+import 'package:hetima/hetima.dart' as hetima;
+import 'package:hetima/hetima_sv.dart' as hetima_sv;
+
+void main() {
+   hetima_sv.HttpServer server = new hetima_sv.HttpServer();
+   server.start();
+}
--- lib/net/httpserver.dart
+++ lib/net/httpserver.dart
@@ -6,7 +6,12 @@ class HttpServer {
   int _port = 8081;
   String _host = "localhost";
 
+  Map<String,Map> redirect = {"/redirect":{"scheme":"http","host":"localhost","port":8081,"path":"/"}};
 
+  /// {"/redirect":{"scheme":"http","host":"localhost","port":8081,"path":"/"}};
+  void putRedirect(String currentPath, String nextScheme, String nextHost, int nextPort, String nextPath) {
+    redirect[currentPath] = {"scheme":nextScheme,"host":nextHost,"port":nextPort,"path":nextPath};
+  }
   void handleError(io.HttpRequest req, io.HttpResponse res) {
     res.statusCode = 404;
     res.close();
@@ -39,9 +44,38 @@ class HttpServer {
     });
   }
 
+  void handleRedirect(io.HttpRequest req, io.HttpResponse res, String path) {
+    Map next = null;
+    for(String r in redirect.keys) {
+      if(r == req.uri.path) {
+        next = redirect[r];
+        break;
+      }
+    }
+    res.statusCode = io.HttpStatus.MOVED_TEMPORARILY;
+    String query = "";
+    if(req.uri.query != null && req.uri.query.length != 0) {
+      query = "?"+req.uri.query;
+    }
+    String location = ""+next["scheme"]+"://"+next["host"]+":"+next["port"].toString()+next["path"]+query;
+    res.headers.set(io.HttpHeaders.LOCATION, "" + location + "");
+    res.headers.set("Content-Type", "text/html");
+    res.close();
+  }
+
   void onListen(io.HttpRequest request) {
-    print("onListen...:" + request.uri.path + "," + request.headers.host);
+    print("onListen...:" + request.uri.scheme+","
+          + request.headers.host+","+request.uri.path + ","
+          + ","+request.uri.port.toString()+","+request.uri.query);
     String path = _rootpath + request.uri.path;
+    // redirect
+    for(String r in redirect.keys) {
+      if(r == request.uri.path) {
+        handleRedirect(request, request.response, request.uri.path);
+        return;
+      }
+    }
+    // file or directory
     io.FileSystemEntity.isDirectory(path).then((isDir) {
       if (isDir == true) {
         handleDir(request, request.response, path);
