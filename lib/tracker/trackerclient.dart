d931861cf3f426ac6e06928e59d93dcb0d317aff
kyorohiro <kyorohiro@gmail.com>
2014-07-06 20:07:41 +0900

    ..

--- lib/tracker/trackerclient.dart
+++ lib/tracker/trackerclient.dart
@@ -35,36 +35,52 @@ class TrackerClient {
   async.Future<RequestResult> request() {
     async.Completer<RequestResult> completer = new async.Completer();
     io.HttpClient client = new io.HttpClient();
-    ArrayBuilder buffer = new ArrayBuilder();
     (new async.Future.sync(() {
       print("--[A0]-" + trackerHost + "," + trackerPort.toString() + "," + path + header);
-      return client.get(trackerHost, trackerPort, path + header)
-      .then((io.HttpClientRequest request) {
+      return client.get(trackerHost, trackerPort, path + header).then((io.HttpClientRequest request) {
         print("--[A1]-");
-        return request.close().then((io.HttpClientResponse response) {
-          print("--[A2]-");
-          return response.listen((List<int> contents) {
-            print("--[A3]-" + contents.runtimeType.toString());
-            print("listen:" + contents.length.toString());
-            print("ret:" + convert.UTF8.decode(contents.toList()));
-            buffer.appendUint8List(contents, 0, contents.length);
-          })
-          .onDone(() {
-            print("--[A4]-");
-            print("done");
-            //TrackerResponse response = new TrackerResponse.bencode(buffer.toUint8List());
-            TrackerResponse response = new TrackerResponse();
-            completer.complete(new RequestResult(response, RequestResult.OK));
-          });
-        });
+        return request.close().then((io.HttpClientResponse response) /**/
+        {
+          responseHandle(response, completer);
+        }/**/
+        );
       });
-    })).catchError((e){
-      completer.complete(new RequestResult(null,RequestResult.ERROR));
+    })).catchError((e) {
+      completer.complete(new RequestResult(null, RequestResult.ERROR));
       print("##er end");
-    }).then((e){
+    }).then((e) {
       print("###done end");
     });
     return completer.future;
+  }
+
+  void responseHandle(io.HttpClientResponse response, async.Completer<RequestResult> completer) /**/
+  {
+    print("--[A2]-");
+    int redirectNum = 0;
+    if(null != response.redirects && response.redirects.length>0) {
+      response.redirect().then((io.HttpClientResponse response) {
+        redirectNum++;
+        if(redirectNum>5) {
+          completer.complete(new RequestResult(null, RequestResult.ERROR));
+        }
+        responseHandle(response, completer);
+      });
+      return;
+    }
+    ArrayBuilder buffer = new ArrayBuilder();
+    response.listen((List<int> contents) {
+      print("--[A3]-" + contents.runtimeType.toString());
+      print("listen:" + contents.length.toString());
+      print("ret:" + convert.UTF8.decode(contents.toList()));
+      buffer.appendUint8List(contents, 0, contents.length);
+    }).onDone(() {
+      print("--[A4]-");
+      print("done");
+      TrackerResponse response = new TrackerResponse.bencode(buffer.toUint8List());
+      //TrackerResponse response = new TrackerResponse();
+      completer.complete(new RequestResult(response, RequestResult.OK));
+    });
   }
 }
 
